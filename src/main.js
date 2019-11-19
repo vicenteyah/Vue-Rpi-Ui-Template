c0004bc0c18cca16c72bc1e9100af9486d4e1cb8
vicente yah <vicente.yah.dzul@icloud.com>
2019-11-18 18:46:37 -0600

    bootstrapvue imported and used

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import BootstrapSocial from 'bootstrap-social';
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import { router } from './router';
+Vue.use(BootstrapSocial)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 
